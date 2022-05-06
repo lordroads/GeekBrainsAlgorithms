342dbb603025f29c383f658135949bf2f3efbc85
Yurii Mitrohin <lordroads@mail.ru>
2022-05-06 19:48:48 +0300

    Решение домашнего задания.

---
+++ Lessons-5/Tree_DFS_BFS/TreeNode.cs
@@ -0,0 +1,13 @@
+﻿namespace Tree_DFS_BFS;
+
+public class TreeNode
+{
+    public int Value { get; set; }
+    public TreeNode? Left { get; set; }
+    public TreeNode? Rieght { get; set; }
+
+    public TreeNode(int value)
+    {
+        Value = value;
+    }
+}
