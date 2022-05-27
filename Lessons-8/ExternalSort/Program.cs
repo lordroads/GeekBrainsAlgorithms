1f6e849b8718b86327edbf449a5547331886aec7
Yurii Mitrohin <lordroads@mail.ru>
2022-05-27 17:17:07 +0300

    Решение второго задания.

---
+++ Lessons-8/ExternalSort/Program.cs
@@ -0,0 +1,7 @@
+﻿using Sorts;
+
+string inFileName = "dataIn_1000000.txt"; // 1 000 000 lines = 3s.
+
+Console.WriteLine($"NOT Sort array - stast");
+Utils.ExternalSort(inFileName);
+Console.WriteLine($"Sort array - done");
